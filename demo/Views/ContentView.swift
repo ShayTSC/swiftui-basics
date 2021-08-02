886214d1efd25e5d12f5a8ac7a2131184f8f9137
abnernat <abnernat@outlook.com>
2021-08-02 17:39:21 +0800

    feat(all): add model data and environment object content

---
+++ demo/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by abnernat on 7/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
