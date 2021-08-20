215b1520840b70f1206a3be03049ad5c94364afb
abnernat <abnernat@outlook.com>
2021-08-20 14:42:40 +0800

    feat(all): animations, etc

---
+++ demo/Views/Badge/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  demo
+//
+//  Created by abnernat on 8/2/21.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
